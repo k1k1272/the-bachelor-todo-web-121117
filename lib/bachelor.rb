f97ed4263932b185ae5aaacb57708ba17ae8225c
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 02:22:57 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,9 @@ def get_contestant_name(data, occ)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  data.map { |year, girls| girls.map { |girl| girl["name"] if girl["hometown"] == hometown }.compact - []}
+  count = 0
+  data.map { |year, girls| girls.map { |girl| count += 1if girl["hometown"] == hometown }.compact}
+  count
 end
 
 def get_occupation(data, hometown)
