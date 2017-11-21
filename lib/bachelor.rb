f4c9191e758faeedd5768a5fce17b61d45bb0fe3
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 02:18:14 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.map { |year, girls| girls.each { |girl| return girl["name"].split(" ").first if year == season && girl["status"] == "Winner"} }
 end
 
-def get_contestant_name(data, occupation)
-  data.map {}
+def get_contestant_name(data, occ)
+  data.map { |year, girls| girls.each { |girl| return girl["name"] if girl["occupation"] == occ }}
 end
 
 def count_contestants_by_hometown(data, hometown)
