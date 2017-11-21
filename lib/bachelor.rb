202af4b6e8b75e2d06726361b9c76eaa76cc035c
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 02:07:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,11 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  final = ""
   data.map do |year, girls|
     girls.map do |girl|
-      final = girl[:name].split(" ").first if year.to_s == season && girl[:status] == "Winner"
+      return girl[:name].split(" ").first if year.to_s == season && girl[:status] == "Winner"
     end
   end
-  final
 end
 
 def get_contestant_name(data, occupation)
